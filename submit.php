d8ac4d62b5e4ebf1346a3296de654ce50231878f
Brien Coffield <coffbr01@coffbr01-laptop.(none)>
2011-11-17 22:44:46 -0600

    success message and more appropriate subject/body

--- submit.php
+++ submit.php
@@ -347,9 +347,10 @@ $mail = new EMail();
 //$mail->setTo("create@philipjordandesign.com");
 $mail->setTo("coffbr01@gmail.com");
 $mail->setFrom("coffbr01@gmail.com");
-$mail->setSubject("Think I got it working ...");
-$mail->setMessage("You should see a PDF as an attachment");
+$mail->setSubject("This is the subject of the email");
+$mail->setMessage("This is the body of the email");
 $mail->setAttachName($_FILES["fileInput"]["name"]);
 $mail->setAttachFileName($_FILES["fileInput"]["tmp_name"]);
-$mail->SendMail();
+$result = $mail->SendMail();
+echo $result[1];
 ?>
